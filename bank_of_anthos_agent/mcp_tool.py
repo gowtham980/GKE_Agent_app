d48d1b79272adeda6dcd1d19669e805ad1fc1b58
GowthamKrishnaTeja <gowthamkrishnateja@gmail.com>
2025-09-23 03:07:44 +0530

    Initial commit of Aura Financial Advisor project

--- bank_of_anthos_agent/mcp_tool.py
+++
@@ -1,7 +0,0 @@
-from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StreamableHTTPConnectionParams
-
-toolset = MCPToolset(
-    connection_params=StreamableHTTPConnectionParams(
-        url="https://mcp.fi.money:8080/mcp/stream"
-    )
-)
